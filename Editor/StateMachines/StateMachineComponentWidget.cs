b096170abfabbffb8cf1fd9fedb33757739c8528
James King <james.king@facepunchstudios.com>
2024-08-05 14:54:55 +0100

    StateMachineComponentWidget

---
+++ Editor/StateMachines/StateMachineComponentWidget.cs
@@ -0,0 +1,30 @@
+﻿using Editor;
+using Sandbox.UI;
+using Button = Editor.Button;
+
+namespace Sandbox.Events.Editor;
+
+[CustomEditor( typeof(StateMachineComponent)) ]
+public sealed class StateMachineComponentWidget : ComponentEditorWidget
+{
+	public StateMachineComponentWidget( SerializedObject obj ) : base( obj )
+	{
+		Layout = Layout.Column();
+		Layout.Margin = new Margin( 30f, 20f );
+
+		RebuildUI();
+	}
+
+	public void RebuildUI()
+	{
+		Layout.Add( new Button( "Open in Editor", "edit" )
+		{
+			Clicked = OnOpenEditor
+		} );
+	}
+
+	private void OnOpenEditor()
+	{
+		Log.Info( "Hi!" );
+	}
+}
